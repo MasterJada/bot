3bfc6ebb69d12ba1b9869ad82ae4423aa18a5c74
Oleg <oleg.r@didww.com>
2019-08-12 12:06:10 +0300

    add express

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var TelegramBotApi = require('node-telegram-bot-api')
-var http = require('http');
-var fs = require('fs');
+var express = require('express');
+var bodyParser = require('body-parser')
+var app = express();
 var request = require('request');
+
+app.use(bodyParser.urlencoded({ extended: false }))
 
 var token = "937074570:AAEydYJbz4U2Q6cqLM_YTiK1UjjYpTWRgMA";
 const tBot = new TelegramBotApi(token, {polling: true});
@@ -25,20 +28,19 @@ const subscribers = [];
         })
   });
 
-  var download = function(uri, filename, callback){
-    request.head(uri, function(err, res, body){
-      console.log('content-type:', res.headers['content-type']);
-      console.log('content-length:', res.headers['content-length']);
-            
-      request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-    });
-  };
-  // var port = process.env.PORT || 4000;
-  // http.createServer((req, resp) =>{
-  //   resp.writeHead(200, {'Content-Type': 'text/html'});
-  //   resp.end('Hello World!');
-  // }).listen(port)
+  app.post("/test",(req, resp) =>{
+    console.log(req.body.url);
+    resp.send("Hello!")
+  })
 
+  app.get("/",(req, resp) =>{
+      resp.send("<h1>Ok it's works</h1>")
+  })
+
+var port = process.env.PORT || 4000;
+app.listen(port,()=> {
+  console.log("Server run on " + port);
+})
 
   tBot.onText(/\unsubscribe/, (msg)=>{
         const chatId = msg.chat.id
@@ -49,3 +51,5 @@ const subscribers = [];
             console.log(subscribers);
         }
   });
+
+  //curl -d "url=google.com" -X POST http://localhost:4000/test
