18e87a367428986e8f519a72f298a77477011150
olegreksa <oleg@gr-development.com>
2019-08-18 20:35:01 +0300

    Fix bug with bot

--- index.js
+++ index.js
@@ -7,15 +7,10 @@ var fs = require('fs')
 var replace = require('buffer-replace');
 
 var db = require('./db')
-var tBot = require('./bot')
+var tBot = require('./bot').tBot
 app.use(express.static("front"))
 app.use(bodyParser.urlencoded({ extended: false }))
 
-
-  app.post("/test",(req, resp) =>{
-    console.log(req.body.url);
-    resp.send("Hello!")
-  });
 
   app.post('/deploy', (req, resp) =>{
       var url = req.body.url;
@@ -29,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
             var url = element.url
             if(url.endsWith('apk')){
               db.getSubscribers(project, (subscribers) =>{
-                subscribers.forEach(chatId => 
+                subscribers.forEach(chatId =>
                   tBot.sendMessage(chatId, url))
               })
             }
@@ -45,7 +40,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
   app.get("/projects", (req, resp) =>{
     db.getAllProjects((projects)=>{
-      if(projects){ 
+      if(projects){
         resp.json(projects)
       }else{
         resp.json([])
@@ -54,7 +49,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
   });
 
   app.post("/addproject", (req, resp)=>{
-    var project = {name: req.body.name} 
+    var project = {name: req.body.name}
     if(project){
       db.addProject(project, (callback) =>{
         resp.json(callback)
@@ -73,7 +68,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
     resp.json(callback)
    })
  });
-  
+
 app.get('/config.yml', (req, resp) =>{
     var prjID = req.query.id
     if(!prjID) return resp.sendStatus(404)
@@ -88,7 +83,7 @@ var port = process.env.PORT || 4000;
 })
 
 
- 
+
 
   //curl -d "url=https://google.com" -X POST http://localhost:4000/test
   //curl -d "url=https://circleci.com/api/v1.1/project/github/Mas/20/artifacts?circle-token=7971292077885be4d63a38abd0d79eef2b8d0d8d" -X POST https://olegscdbot.herokuapp.com/deploy
